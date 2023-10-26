e30270468c24ba617e4ef5b2853410768ddce40d
class moon <57883335+OrphanZhao@users.noreply.github.com>
2023-10-26 17:57:00 +0800

    feat: token validate

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -3,36 +3,63 @@ require "test_helper"
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    @user2 = users(:two)
   end
 
   test "index_success: should show users" do
-    get api_v1_users_path, as: :json
+    get api_v1_users_path, 
+      # 新增
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+      as: :json
     assert_response 200
   end
 
-  test "show_success: should show user" do
-    get api_v1_user_path(@user), as: :json
-    json_response = JSON.parse(self.response.body)
-    assert_response 200
-    assert_equal @user.email, json_response["data"]["email"]
-  end
-
-  test "create_success: should create user" do
-    assert_difference("User.count", 1) do
-      post api_v1_users_path, params: { user: { email: "test@test.com", password: "123456" } }, as: :json
-    end
-    assert_response 201
-  end
+  # 未修改代码
 
   test "update_success: should update user" do
-    put api_v1_user_path(@user), params: { user: { email: "test@test.com", password: "123456" } }, as: :json
+    put api_v1_user_path(@user), 
+      params: {user:{email: 'test1@test.com', password: '123456'}},
+      # 新增
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+      as: :json
+  
     assert_response 202
   end
 
-  test "delete_success: should delete user" do
+  test "destroy_success: should destroy user" do
+    # 验证 User.count 变化 -1
     assert_difference('User.count', -1) do
-      delete api_v1_user_path(@user), as: :json
+      delete api_v1_user_path(@user2), 
+        # 新增
+        headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+        as: :json
     end
+
     assert_response 204
+  end
+
+  test "index_forbidden: should forbiden show users cause not admin" do
+    get api_v1_users_path, 
+    # 新增
+    headers: { Authorization: JsonWebToken.encode(user_id: @user2.id) },
+    as: :json
+    assert_response 403
+  end
+
+  test "update_forbidden: should forbiden update user cause not admin" do
+    put api_v1_user_path(@user), 
+    params: {user:{email: 'test1@test.com', password: '123456'}},
+    headers: { Authorization: JsonWebToken.encode(user_id: @user2.id) },
+    as: :json
+
+    assert_response 403
+  end
+
+  test "destroy_forbidden: should forbiden destroy_ user cause not admin" do
+    delete api_v1_user_path(@user), 
+    headers: { Authorization: JsonWebToken.encode(user_id: @user2.id) },
+    as: :json
+
+    assert_response 403
   end
 end
