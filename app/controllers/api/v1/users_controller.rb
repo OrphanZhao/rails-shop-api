b94e35d86f01954a51663a10c534815bf6152580
class moon <57883335+OrphanZhao@users.noreply.github.com>
2023-10-26 14:58:44 +0800

    feat: User model index

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::UsersController < ApplicationController
+    before_action :set_per_page, only: [:index]
+    before_action :set_page, only: [:index]
+
+    def index
+        @users = User.offset(@page).limit(@per_page)
+        render json: { err_code: 0, data: @users, message: "ok" }, status: 200
+    end
+
+    private
+    
+    def _to_i(param, default_no = 1)
+        param && param&.to_i ? param&.to_i : default_no.to_i
+    end
+
+    def set_page
+        @page = _to_i(params[:page], 1)
+        @page = set_per_page * (@page - 1)
+    end
+
+    def set_per_page
+        @per_page = _to_i(params[:per_page], 10)
+    end
+end
